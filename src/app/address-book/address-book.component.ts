3ddf095e7f77490c13d0958524c28cbd5671117e
Giovanni Albano <giovanni.albano1995@libero.it>
2022-11-26 17:52:18 +0100

    #004 handle edit scenario

--- src/app/address-book/address-book.component.ts
+++ src/app/address-book/address-book.component.ts
@@ -1,7 +1,11 @@
+import { FormBuilder, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Subscription, Observable } from 'rxjs';
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
+
 import { Contacts } from '@model/interfaces';
+
+import { Subscription, Observable } from 'rxjs';
+
 
 @Component({
   selector: 'address-book',
@@ -10,16 +14,34 @@ import { Contacts } from '@model/interfaces';
 })
 export class AddressBookComponent implements OnInit, OnDestroy {
 
+  contactForm = this.fb.group({
+    name: ['', Validators.required],
+    iban: ['', Validators.required],
+    phone: ['', Validators.required],
+    email: ['', Validators.required],
+  })
+
   private subscriptions = new Subscription();
 
+  @ViewChild('dialogForm', {static: true}) dialogForm: ElementRef;
+
   title = 'Address Book';
+  modalTitle = 'Add Contact';
+  confirmButtonLabel = 'Save';
   tableHeader: Array<string> = ['Name', 'IBAN', 'Phone', 'Email', 'Actions'];
   contacts : Array<Contacts> = [];
-  constructor(private http: HttpClient) {}
+  isEdit = false;
+  editContactId: number;
+
+  constructor(private http: HttpClient, private fb: FormBuilder) {}
+  get dialog(): any {
+    return this.dialogForm.nativeElement;
+  }
 
   ngOnInit(): void {
       this.subscriptions.add(this.http.get<Observable<Contacts>>('http://localhost:3000/api/contacts').subscribe((data) => {
         this.contacts = data['data']['contacts'];
+        localStorage.setItem('contacts', JSON.stringify(this.contacts));
       }))
   }
 
@@ -44,13 +66,62 @@ export class AddressBookComponent implements OnInit, OnDestroy {
     this.setAriaHidden(id, toggle);
 }
 
+  addContact() {
+    this.isEdit = false;
+    this.modalTitle = 'Add Contact';
+    this.confirmButtonLabel = 'Save';
+    this.showModal();
+  }
+
+  editContact(index: number) {
+    this.isEdit = true;
+    this.modalTitle = 'Edit Contact';
+    this.confirmButtonLabel = 'Update';
+    this.editContactId = index;
+    this.contactForm.controls["name"].setValue(this.contacts[index].name);
+    this.contactForm.controls["iban"].setValue(this.contacts[index].iban);
+    this.contactForm.controls["phone"].setValue(this.contacts[index].phone);
+    this.contactForm.controls["email"].setValue(this.contacts[index].email);
+    this.showModal();
+  }
 
   removeContact(index: number) {
    this.contacts.splice(index, 1);
   }
 
+  showModal() {
+    this.dialog.classList.add('fd-dialog--active');
+  }
+
+  hideModal() {
+    this.dialog.classList.remove('fd-dialog--active')
+    this.contactForm.reset();
+  }
+
+  addNewContact() {
+    var newContact = this.contactForm.value;
+    newContact['id'] = this.contacts[this.contacts.length - 1].id + 1;
+    this.contacts.push(newContact as Contacts);
+  }
+
+  saveEditContact() {
+    this.contacts[this.editContactId] = {
+      id: this.editContactId,
+      name: this.contactForm.value.name,
+      iban: this.contactForm.value.iban,
+      phone: this.contactForm.value.phone,
+      email: this.contactForm.value.email
+    }
+  }
+
+  onSubmit() {
+    this.isEdit ? this.saveEditContact() : this.addNewContact();
+    this.hideModal();
+  }
+
   ngOnDestroy(): void {
       this.subscriptions.unsubscribe();
+      localStorage.clear();
   }
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -10,14 +11,15 @@ import { PaymentSummaryComponent } from './payment-summary/payment-summary.compo
 import { AppRoutingModule } from './app-routing.module';
 
 
+
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     PaymentSummaryComponent,
-    AddressBookComponent
+    AddressBookComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
